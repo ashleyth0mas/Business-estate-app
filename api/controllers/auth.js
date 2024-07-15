ebc63e18fec0c9376ae285415f7b1db08bb445ba
Ashley Thomas <115642737+ashleyth0mas@users.noreply.github.com>
2024-07-16 00:14:35 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -6,12 +6,12 @@ import jwt from 'jsonwebtoken'
 import dotenv from "dotenv";    
 dotenv.config()
 export const signup=async(req,res,next)=>{
-    const{username,email,password}=req.body;   //accessing data from post request and assign to variables {username,email,password} (This line is just variable assignment)
-    const hashedPassword=bcryptjs.hashSync(password,10);  // to encrypt password (in database)
-    const newUser=new User({username,email,password:hashedPassword}); //creating a instance of usermodel and this instance contains the user model entity 
+    const{username,email,password}=req.body;   
+    const hashedPassword=bcryptjs.hashSync(password,10); 
+    const newUser=new User({username,email,password:hashedPassword}); 
     try{
-        await newUser.save();            //saving the user entity to database
-        res.status(201).json('User created successfully!!'); //this is returned to fetch call otherwise error
+        await newUser.save();          
+        res.status(201).json('User created successfully!!'); 
     }
     catch(err){
         next(err);
@@ -22,26 +22,17 @@ export const signup=async(req,res,next)=>{
 export const signin=async(req,res,next)=>{
     const {email,password}=req.body;
     try{
-    const validUser=await User.findOne({email})    // to search for a user in the database with the email address provided in the request body.
-                                                  //If a user is found matching the email criteria, findOne returns the JavaScript object representation of the entire user document retrieved from the database
-    if (!validUser){                               
-        return next(errorHandler(404,'User not found!'))
+    const validUser=await User.findOne({email})    
+                                                  
         }
-    const validPassword=bcryptjs.compareSync(password,validUser.password)    //bcryptjs.compareSync(password, validUser.password): This line (assuming you're using the bcryptjs library) compares the password entered by the user (password from the request body) with the hashed password stored for the valid user (validUser.password).
+    const validPassword=bcryptjs.compareSync(password,validUser.password)
     if(!validPassword)
         return next(errorHandler(401,'Wrong credentials!'));
-    const token=jwt.sign({email:validUser.email,id:validUser._id},process.env.JWT_SECRET_KEY) //jwt.sign(payload, secretOrPrivateKey, [options, callback]): This function generates a JSON Web Token (JWT) using the provided payload (data to be stored in the token), secret key (used to sign the token), and options (such as the token expiration time).
+    const token=jwt.sign({email:validUser.email,id:validUser._id},process.env.JWT_SECRET_KEY)
     const  {password:pass,...rest}=validUser._doc     
     
-    /* LINE 34 EXPLANATION
-    -----------------------
-    Object Destructuring:
 
-The left-hand side (const { password: pass, ...rest } = validUser._doc;) defines a new constant variable named rest and another variable named pass.
-The password: pass syntax assigns the value of the password property from validUser._doc to the pass variable. This part is optional; you could use password directly if you don't need a different name.
-The ...rest part (...) is the "rest" operator. It captures all remaining properties from validUser._doc (excluding password) and assigns them to the rest object.
-    */
-    res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)    // sets an HTTP cookie named access_token with the value of the generated JWT (token). Cookies are a common method for storing tokens, particularly JWTs (JSON Web Tokens), in web applications. , the res.cookie() function is indeed used for creating cookies to be sent along with the HTTP response.
+    res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)   
     }
     catch(error){
         next(error)
@@ -57,10 +48,10 @@ export const google=async(req,res,next)=>{
           res.cookie('access_token',token,{httpOnly:true}).status(200).json(rest)
         }
         else{   //USER SIGN IN WITH GOOGLE FOR THE FIRST TIME
-            const generatedPassword=Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);  // to generate random password ,if the user is signing via google for the first time bcoz our usermodel has a defined structure(username,email,password etc)
+            const generatedPassword=Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);  
             const hashedPassword=bcryptjs.hashSync(generatedPassword,10);
             const newUser=new User({
-                username:req.body.name.split(" ").join("").toLowerCase(),  //creating the username from the name extracted from req body ( which is the google account name obtained from google authentication)
+                username:req.body.name.split(" ").join("").toLowerCase(),  
                 email:req.body.email,
                 password:hashedPassword,
                 avatar:req.body.photo
