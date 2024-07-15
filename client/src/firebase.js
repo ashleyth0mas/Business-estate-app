d416f2b3e8d222bb0e5bb27355369f1cc5beb903
Ashley Thomas <115642737+ashleyth0mas@users.noreply.github.com>
2024-07-16 00:38:56 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+import { initializeApp } from "firebase/app";
+
+
+
 const firebaseConfig = {
   apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-estate-8459a.firebaseapp.com",
@@ -13,5 +12,5 @@ const firebaseConfig = {
   appId: "1:30166199552:web:3f9a0c4fff37872fb997e5"
 };
 
-// Initialize Firebase
+
 export const app = initializeApp(firebaseConfig);
