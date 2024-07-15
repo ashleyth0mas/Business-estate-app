974441fd7d056a75e492fcccc263a9082d689403
Ashley Thomas <115642737+ashleyth0mas@users.noreply.github.com>
2024-07-16 00:28:35 +0530

    Update user.model.js

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,6 @@ const userSchema=new mongoose.Schema({
     },
 
 },{timestamps:true});
-//creates the usermodel with the schema specified
+
 const User=mongoose.model('User',userSchema);
 export default User;
